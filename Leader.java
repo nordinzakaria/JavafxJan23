5e6006883870a6f0d2e3e1f3b9d8305f7abc3a90
unknown <nordinzakaria@gmail.com>
2023-03-28 09:46:42 +0200

    added Leader class

---
+++ Leader.java
@@ -0,0 +1,17 @@
+package com.example.javafxdemo;
+
+public class Leader {
+    private String name;
+
+    public Leader(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
